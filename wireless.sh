73f24025d29605a3c869e833debae2a09bfbcdff
royal <137996547+ritascarlet@users.noreply.github.com>
2025-10-14 18:15:31 +0300

    Update wireless.sh

--- wireless.sh
+++ wireless.sh
@@ -1,6 +1,51 @@
 #!/bin/sh
 
-cat > /etc/config/wireless << 'EOF'
+WIRELESS_CONFIG="/etc/config/wireless"
+
+
+OPENWRT_VERSION=$(cat /etc/os-release | grep 'OPENWRT_RELEASE=' | cut -d'"' -f2 | cut -d'.' -f1)
+
+
+cp "$WIRELESS_CONFIG" "$WIRELESS_CONFIG.bak" 2>/dev/null
+
+
+if [ "$OPENWRT_VERSION" = "24" ]; then
+    cat > "$WIRELESS_CONFIG" << 'EOF'
+config wifi-device 'radio0'
+        option type 'mac80211'
+        option path 'platform/soc/18000000.wifi'
+        option channel '1'
+        option band '2g'
+        option htmode 'HE20'
+        option cell_density '0'
+
+config wifi-iface 'default_radio0'
+        option device 'radio0'
+        option network 'lan'
+        option mode 'ap'
+        option ssid 'OfficialVPN_2G'
+        option encryption 'psk2'
+        option key '12345678'
+
+config wifi-device 'radio1'
+        option type 'mac80211'
+        option path 'platform/soc/18000000.wifi+1'
+        option band '5g'
+        option channel '36'
+        option htmode 'HE80'
+        option cell_density '0'
+
+config wifi-iface 'default_radio1'
+        option device 'radio1'
+        option network 'lan'
+        option mode 'ap'
+        option ssid 'OfficialVPN_5G'
+        option encryption 'psk2'
+        option key '12345678'
+EOF
+    echo "OS-Release 24 in $WIRELESS_CONFIG"
+else
+    cat > "$WIRELESS_CONFIG" << 'EOF'
 config wifi-device 'radio0'
         option type 'mac80211'
         option path 'platform/18000000.wifi'
@@ -37,3 +82,5 @@ config wifi-iface 'default_radio1'
         option encryption 'psk2'
         option key '12345678'
 EOF
+    echo "OS-Release 23 in $WIRELESS_CONFIG"
+fi
