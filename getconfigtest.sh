98b6baf191e3daa55bf7981f7bb13ccb8ee9ce2b
kurushmal <137996547+ritascarlet@users.noreply.github.com>
2026-02-28 16:12:23 +0300

    Update getconfigtest.sh

--- getconfigtest.sh
+++ getconfigtest.sh
@@ -129,6 +129,16 @@ add_firewall_rule 'VoIP' "config rule
 	option family 'ipv4'
 	option dest_port '5060 5061'"
 
+add_firewall_rule 'Cloudflare' "config rule
+	option name 'Telegram VPN'
+	option src 'lan'
+	option dest '*'
+	option proto 'all'
+	option set_mark '0x1'
+	option target 'MARK'
+	option family 'ipv4'
+	option dest_ip '173.245.48.0/20 103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 141.101.64.0/18 108.162.192.0/18 190.93.240.0/20 188.114.96.0/20 197.234.240.0/22 198.41.128.0/17 162.158.0.0/15 104.16.0.0/13 104.24.0.0/14 172.64.0.0/13 131.0.72.0/22'"
+
 /etc/init.d/firewall restart
 
 rm -f /etc/sing-box/config.json
